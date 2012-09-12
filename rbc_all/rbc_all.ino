83f489ee588a6310559cd0f5248a913f9878cab7
ryought <ryonakabayashi@gmail.com>
2012-09-12 23:39:25 +0900

    update

--- rbc_all/rbc_all.ino
+++ rbc_all/rbc_all.ino
@@ -1,7 +1,7 @@
 #define STEPS 72  //for kopal 1702
 #define CLOSE_DEG 20
 #define OPEN_DEG 101
-#define MOTOR_SPEED 20z
+#define MOTOR_SPEED 20
 #define TURN_DELAY 1000
 #include <Servo.h>
 
@@ -15,7 +15,7 @@ int servo_status[4]={};
 int motor_pin[4][4]={{16,17,18,19},{8,9,10,11},{12,13,14,15},{0,1,2,3}};
 //このへんは要調整。0,1は使えないみたい。
 
-char turn_symbols[20] = {0};
+char turn_symbols[60] = {0};
 
 /*
 ARM_LAYOUT
@@ -98,6 +98,11 @@ void stepper_control(int motor_num, int rot) {
   if(rot > 0) {
     forward(motor_num, rot);
   }
+  Serial.print("MOVING motor_num : ");
+  Serial.print(motor_num);
+  Serial.print(" rot : ");
+  Serial.print(rot);
+  Serial.println(" ");
 }
 
 void cube_hold(int motor_num, boolean lock_status) { //cube_hold(1, true)で1番を閉じる
@@ -147,6 +152,7 @@ void setup() {
   }
 
   //servo
+  
   servo1.attach(servo_pin[0]);
   servo2.attach(servo_pin[1]);
   servo3.attach(servo_pin[2]);
@@ -163,13 +169,15 @@ void recvCmd(char *buf) {
   while(1) {
     if(Serial.available() > 0) {
       c = Serial.read();
-      if(c == '\n') {  //改行コードは飛ばす
+      if(c == '\n') {  //改行コードは飛ばす and 終端文字としての\n
         Serial.println("Line feed code was detected");
         break;
       } else {  
         buf[count] = c;
       }
+      /*
       if(c == '%') break;  //終端記号%でルーブ脱出
+      */
       count++;
     }
   }
@@ -186,22 +194,14 @@ void recvCmd(char *buf) {
 
 void define_symbols() {
   String def_symbols = turn_symbols;
-  //Serial.println(def_symbol.charAt(0));
-  int i=0;
-  /*
-  switch(def_symbols.charAt(i)) {
-    case 'F'
-    case 'B':
-    case 'R':
-    case 'L':
-    case '\'':
-    case '(':
-    case ' ':
-  }
-  */
-  for(int i=1; i<=20; i++) {
+  Serial.println(def_symbols.charAt(0));
+
+  for(int i=0; i<=60; i++) {
+    Serial.print("seeking at : ");
+    Serial.println(i);
     switch(def_symbols.charAt(i)) {
       case 'F': //1
+        Serial.println("found : F");
         //回す処理＋ホールド処理
         cube_hold(1, true);
         cube_hold(2, true);
@@ -219,6 +219,7 @@ void define_symbols() {
         }
         break;
       case 'B': //3
+        Serial.println("found : B");
         //回す処理＋ホールド処理
         cube_hold(1, true);
         cube_hold(2, true);
@@ -236,6 +237,7 @@ void define_symbols() {
         }
         break;
       case 'R'://4
+        Serial.println("found ; R");
         //回す処理＋ホールド処理
         cube_hold(1, true);
         cube_hold(2, true);
@@ -253,6 +255,7 @@ void define_symbols() {
         }
         break;
       case 'L'://2
+        Serial.println("found : L");
         //回す処理＋ホールド処理
         cube_hold(1, true);
         cube_hold(2, true);
@@ -271,10 +274,12 @@ void define_symbols() {
         break;
       
       case '\'' | '2' | ' ' | ')':
+        Serial.println("found : ', 2,  , )");
         //逆向きに回す処理　　次の文字を読んで判定
         continue;
         
       case '(':
+        Serial.println("found : (");
         //一周回転用
         switch(def_symbols.charAt(i+1)) {
           case 'f':
@@ -307,8 +312,67 @@ void define_symbols() {
     
 
 void loop() {
+  char commands[50]; //コマンド用バッファ
+  
+  commands[0] = 0;
+  recvCmd(commands); //受信コマンドをcommandsに
+  
+  Serial.print("command[0] : ");
+  Serial.println(commands[0]); 
+  if(commands[0] == '$') { //先頭が$ならPCから送られたコマンド
+    Serial.println("command recieved");
+    
+    int command_length = commands[1] + (commands[2] * 256); //コマンド長計算
+    Serial.print("command length : ");
+    Serial.println(command_length);
+
+    switch(commands[3]) {  //コマンドにより分岐
+      case 0x10:
+        Serial.println("CMD : start turning");
+        for(int inc=0; inc<20; inc++) {
+          Serial.print("received : ");
+          Serial.println(turn_symbols[inc]);   
+        }
+        Serial.println("CMD : exit");
+        //define_symbols();
+        define_symbols();
+        break;
+      case 0x1F:
+        Serial.println("CMD : stop turning");
+        break;
+      case 0x20:
+        Serial.println("CMD : start capturing");
+        
+        break;
+      case 0x2F:
+        Serial.println("CMD : stop captureing");
+        break;
+      case 0x80:
+        Serial.println("CMD : recieve turning symbol");
+        //char turn_symbols[command_length-5];
+        for(int inc=0; inc<=command_length-6; inc++) {
+          turn_symbols[inc] = commands[inc+4];
+          Serial.print("received : ");
+          Serial.println(turn_symbols[inc]);   
+          //commands配列から回転記号を抽出し、turn_symbols配列に入れる。
+        }
+        //define_symbols();
+        break;
+    }    
+    
+  } else {
+    Serial.println("unknown command");
+  }
+  
   cube_hold(1, true);
-  stepper_control(1, 3);
-  cube_hold(1, false);
-  stepper_control(1, -3);
+  stepper_control(1, 4);
+  stepper_control(1, -1);
+  cube_hold(1,false);
+  delay(1000);
+  /*
+  cube_hold(1, true);
+  cube_hold(2, true);
+  cube_hold(3, true);
+  cube_hold(4, true);
+  */
 }
